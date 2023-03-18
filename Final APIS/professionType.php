28a96eaef574247edbecb6279f1d31b286af2068
Sanket0608 <sanketugale2003@gmail.com>
2023-03-18 20:03:32 +0530

    commit

---
+++ Final APIS/professionType.php
@@ -0,0 +1,21 @@
+<?php
+include('db.php');
+$data = json_decode(file_get_contents('php://input'), true);
+$profession=$data['profession'];
+$sqlRes = mysqli_query($con, "SELECT * FROM professional WHERE profession='$profession'");
+if(mysqli_num_rows($sqlRes)>0){
+    $data=[];
+    while($row=mysqli_fetch_assoc($sqlRes)){
+        $data[]=$row;
+    }
+    $status='true';
+    $code='5';
+}else{
+    $status='true';
+    $data="Data not found";
+    $code='4';
+}
+echo json_encode(["status"=>$status,"data"=>$data,"code"=>$code]);
+
+
+?>
